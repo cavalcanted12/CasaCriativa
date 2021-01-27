c31c08e344713c76e4fe2b1344fd185a4e961c3d
RHUAN CAVALCANTE DA SILVA <cavalcante.d12@gmail.com>
2021-01-26 23:40:45 -0400

    CasaCriativa

---
+++ server.js
@@ -0,0 +1,26 @@
+ // usei o express para criar e configurar meu servidor
+const express = require("express")
+const server = express()
+
+// configurar arquivos estáticos (css, scripts, imagens)
+server.use(express.static("public"))
+
+// configuração do nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+    express: server,
+    noCache: true, // boolean
+})
+
+// criei uma rota '/'
+// e capturo o pedido do cliente para responder
+server.get("/", function(req, res) {
+    return res.render("index.html")
+})
+
+server.get("/ideias", function(req, res) {
+    return res.render("ideias.html")
+})
+
+// liguei meu servidor na porta 3000
+server.listen(3000)
